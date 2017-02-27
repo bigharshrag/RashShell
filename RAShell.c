516101267620eb8f77a94db32c5be1ff7febb81d
bigharshrag <garg.rishabh22@gmail.com>
2017-02-28 00:33:42 +0530

    Added 1 level redirection

--- RAShell.c
+++ RAShell.c
@@ -3,13 +3,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define TOKEN_BUFF_SIZE 16
+#define CMD_BUFF_SIZE 16
 #define TOKEN_DELIM " \t\r\n\a"
 
 char **get_tokens(char * inp)
 {
-	int buff_size = TOKEN_BUFF_SIZE;
+	int buff_size = CMD_BUFF_SIZE;
 	char **cargs = malloc(buff_size * sizeof(char*)); // command and arguements
 	char *token;
 	int carg_no = 0;
@@ -22,7 +25,7 @@ char **get_tokens(char * inp)
 
 		if(carg_no > buff_size)
 		{
-			buff_size += TOKEN_BUFF_SIZE;
+			buff_size += CMD_BUFF_SIZE;
 			cargs = realloc(cargs, buff_size * sizeof(char*)); // Increase the buffer
 		}
 
@@ -73,26 +76,185 @@ void history_cmd()
 
 }
 
+int custom_cmds(char** cargs)
+{	
+	int ret = 0;
+	// check for known commands
+	if(strcmp(cargs[0], "exit") == 0)
+	{
+		exit_cmd();
+		ret = 1;
+	}
+	else if(strcmp(cargs[0], "cd") == 0)
+	{
+		cd_cmd(cargs[1]);
+		ret = 1;
+	}
+	else if(strcmp(cargs[0], "history") == 0)
+	{
+		history_cmd();
+		ret = 1;
+	}
+	return ret;
+}
+
+int handle_redir(char **cargs)
+{
+
+
+	// char **cmd1, **cmd2, **cmd3; 
+	// cmd1 < cmd2 > cmd3
+	int buff_size = CMD_BUFF_SIZE; // max can be allocated buffer
+	char **cmd1 = malloc(buff_size * sizeof(char*));
+	char **cmd2 = malloc(buff_size * sizeof(char*));
+	char **cmd3 = malloc(buff_size * sizeof(char*));
+	int i = 0, cmd_cnt = 0, pres_flag = 0;
+	int in, out;
+
+	int pid, status, w;
+	int ret;
+
+	while(cargs[i] != NULL)
+	{
+		if(strcmp(cargs[i], "<") == 0 || strcmp(cargs[i], ">") == 0)
+		{
+			pres_flag = 1;
+		}
+		++i;
+	}
+
+	if(pres_flag == 0)
+	{
+		return 0;
+	}
+
+	i = 0;
+
+	while(cargs[i] != NULL)
+	{	
+		if(!(strcmp(cargs[i], "<") == 0 || strcmp(cargs[i], ">") == 0))
+		{
+			// strcpy(cmd1[i], cargs[i]);
+			cmd1[cmd_cnt] = cargs[i];
+			++i;
+			++cmd_cnt;
+
+			if(cmd_cnt > buff_size)
+			{
+				buff_size += CMD_BUFF_SIZE;
+				cmd1 = realloc(cmd1, buff_size * sizeof(char*)); // Increase the buffer
+			}
+		}
+		else
+		{
+			pres_flag = 1;
+			cmd1[cmd_cnt] = NULL;
+			break;
+		}
+	}
+
+	if(pres_flag == 0)
+	{
+		return 0;
+	}
+
+	pid = fork();
+	if(pid == 0)
+	{
+		cmd_cnt = 0;
+		if(strcmp(cargs[i], "<") == 0)
+		{
+			++i;
+			while(cargs[i] != NULL)
+			{	
+				if(!(strcmp(cargs[i], "<") == 0 || strcmp(cargs[i], ">") == 0))
+				{
+					// strcpy(cmd2[cmd_cnt],cargs[i]);
+					cmd2[cmd_cnt] = cargs[i];
+					++i;
+					++cmd_cnt;
+				}
+				else
+				{
+				// 	pres_flag = 1;
+				// 	cmd1[cmd_cnt] = NULL;
+					break;
+				}
+			}
+			in = open(cmd2[0], O_RDONLY);
+			dup2(in, STDIN_FILENO);
+			close(in);
+
+			ret = execvp(cmd1[0], cmd1);
+			if(ret == -1)
+			{
+				perror("rash_redir: command not found");
+				exit(EXIT_FAILURE); // Use over exit(1) as in man page
+			}
+		}
+		else if(strcmp(cargs[i], ">") == 0)
+		{
+			++i;
+			while(cargs[i] != NULL)
+			{	
+				if(!(strcmp(cargs[i], "<") == 0 || strcmp(cargs[i], ">") == 0))
+				{
+					// strcpy(cmd2[cmd_cnt],cargs[i]);
+					cmd2[cmd_cnt] = cargs[i];
+					++i;
+					++cmd_cnt;
+				}
+				else
+				{
+				// 	pres_flag = 1;
+				// 	cmd1[cmd_cnt] = NULL;
+					break;
+				}
+			}
+			out = open(cmd2[0], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+			dup2(out, STDOUT_FILENO);
+			close(out);
+
+			ret = execvp(cmd1[0], cmd1);
+			if(ret == -1)
+			{
+				perror("rash_redir: command not found");
+				exit(EXIT_FAILURE); // Use over exit(1) as in man page
+			}
+		}
+
+	}
+	else if(pid > 0){
+		do {
+            w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
+            if (w == -1) {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	else{
+		printf("Error executing the command. That's all I know.\n");
+	}
+
+	free(cmd1);
+	free(cmd2);
+	free(cmd3);
+	
+	return 1;
+}
+
 void run_command(char** cargs)
 {
 	int pid, status, w;
 	int ret;
 
 
-	// check for known commands
-	if(strcmp(cargs[0], "exit") == 0)
-	{
-		exit_cmd();
-	}
-	else if(strcmp(cargs[0], "cd") == 0)
-	{
-		cd_cmd(cargs[1]);
-	}
-	else if(strcmp(cargs[0], "history") == 0)
-	{
-		history_cmd();
-	}
-	else{
+	if(custom_cmds(cargs) == 1)
+		return;
+	else if(handle_redir(cargs) == 1)
+		return;
+
 	pid = fork();
 	if(pid == 0)
 	{
@@ -115,7 +277,7 @@ void run_command(char** cargs)
 	else{
 		printf("Error executing the command. That's all I know.\n");
 	}
-	}
+	
 }
 
 int main()
